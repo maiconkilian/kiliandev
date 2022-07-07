7cefefa56f5a8d41b2f438c14784959f3aa0f7a4
ivan kiliane <ivankilian@gmail.com>
2022-07-07 10:26:03 -0300

    retirado o nodemon e ajustado conexao banco de dados

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,30 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: '1234',
-        database: 'market_cubos'
+        host: 'ec2-44-206-11-200.compute-1.amazonaws.com',
+        user: 'yzslafmorxjckx',
+        password: '6173b60e0db2c69b85fc0c5a328214edf688138e97ea3430533e1c028c931884',
+        database: 'dbghbv0uovoq3l',
+        port: 5432,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
 module.exports = knex;
+
+
+
+// const knex = require('knex')({
+//     client: 'pg',
+//     connection: {
+//         host: 'localhost',
+//         user: 'postgres',
+//         password: '1234',
+//         database: 'market_cubos'
+//     }
+// });
+
+// module.exports = knex;
+
