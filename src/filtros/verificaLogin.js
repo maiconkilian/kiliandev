8cf6d102147379ee4b4750d92e508236fd39f29e
ivan kiliane <ivankilian@gmail.com>
2022-07-07 08:09:09 -0300

    enviando todos os dados

---
+++ src/filtros/verificaLogin.js
@@ -0,0 +1,35 @@
+const knex = require('../conexao');
+
+
+const jwt = require('jsonwebtoken');
+const senhaHash = require('../senhaHash');
+
+const verificaLogin = async (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        return res.status(401).json('Não autorizado');
+    }
+
+    try {
+        const token = authorization.replace('Bearer ', '').trim();
+
+        const { id } = jwt.verify(token, senhaHash);
+
+        const dados = await knex('usuarios').where('id', id);
+
+        if (dados.length === 0) {
+            return res.status(404).json("O usuario não foi encontrado");
+        }
+
+        const { senha, ...usuario } = dados[0];
+
+        req.usuario = usuario;
+
+        next();
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+module.exports = verificaLogin
